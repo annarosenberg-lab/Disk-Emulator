e786f508fad9ab01e89de2d62c709d804fe7c2ae
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2024-03-19 18:19:00 -0700

    Update libTinyFS.c

--- libTinyFS.c
+++ libTinyFS.c
@@ -453,3 +453,62 @@ int tfs_seek(fileDescriptor FD, int offset) {
     current_entry->offset = offset;
     return 0;
 }
+
+
+//main function
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        printf("Usage: %s <diskname>\n", argv[0]);
+        return 1;
+    }
+
+    char *diskname = argv[1];
+    tfs_mkfs(diskname, DEFAULT_DISK_SIZE);
+
+    int result = tfs_mount(diskname);
+    if (result < 0){
+        printf("Error mounting disk\n");
+        return 1;
+    }
+
+    printf("Disk mounted\n");
+    printf("mountedDiskname: %s\n", mountedDiskname);
+    int mountedFD = openDisk(mountedDiskname, 0);
+    printf("mountedFD: %d\n", mountedFD);
+
+    //create file with tfs_openFile
+    fileDescriptor fd = tfs_openFile("file_01");
+    printf("new file fileDescriptor: %d\n", fd);
+
+    //check if inode was created
+    Inode rootInode;
+    result = readBlock(mountedFD, 1, &rootInode);
+    if (result < 0){
+        printf("Error reading block\n");
+        return 1;
+    }
+    //print inone nexr ptr
+    printf("rootInode.nextInodePtr: %d\n", rootInode.nextInodePtr);
+    
+
+
+    //test tfs_writeByte
+    char *buffer = "Hello, World!";
+    result = tfs_writeFile(fd, buffer, strlen(buffer));
+    if (result < 0){
+        printf("Error writing to file\n, result: %d\n", result);
+        return 1;
+    }
+
+
+
+
+    result = tfs_unmount();
+    if (result < 0){
+        printf("Error unmounting disk\n");
+        return 1;
+    }
+
+
+    return 0;
+} 
